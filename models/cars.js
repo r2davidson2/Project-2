d530aa6bf7cb3654266734101fbc4b2ae0aba156
Ross Davidson <r2davidson2@gmail.com>
2019-08-02 14:29:10 -0700

    models, public and views folders created

---
+++ models/cars.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const carSchema = new mongoose.Schema({
+   year: Number,
+   make: String,
+   model: String,
+   img: String,
+   description: String,
+   
+}, {timestamps:true});
+
+const Cars = mongoose.model('Cars', carSchema);
+
+module.exports = Cars;
